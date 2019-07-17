18536585ac1ecc710f1e67afb9959903a177f5a0
liuchang <63905589@qq.com>
2019-07-17 09:37:11 +0800

    First Commit

---
+++ arcgis_js_api/library/4.11/dojo/src/TiandiImageAnnoLayer.js
@@ -0,0 +1,34 @@
+define(["dojo/_base/declare", "dojo/_base/lang", "esri/config", "esri/layers/BaseTileLayer", "esri/request"],
+	function (declare, lang, esriConfig, BaseTileLayer, esriRequest) {
+		return BaseTileLayer.createSubclass({
+			properties: {
+				urlTemplate: null
+			},
+			getTileUrl: function (level, row, col) {
+				var url = "http://t" + col % 8 + ".tianditu.gov.cn/DataServer?T=cia_w&tk=1b0a9985dc738b91b190888cea15c7ee&x=" + col + "&y=" + row + "&l=" + level;
+				return url;
+			},
+			fetchTile: function (level, row, col) {
+				var url = this.getTileUrl(level, row, col);
+				return esriRequest(url, {
+						responseType: "image"
+					})
+					.then(function (response) {
+
+						var image = response.data;
+						var width = this.tileInfo.size[0];
+						var height = this.tileInfo.size[0];
+
+						var canvas = document.createElement("canvas");
+						var context = canvas.getContext("2d");
+						canvas.width = width;
+						canvas.height = height;
+
+						context.drawImage(image, 0, 0, width, height);
+
+						return canvas;
+					}.bind(this));
+			}
+		});
+	}
+)
